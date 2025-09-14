5cff6a6bca00de17268adb21debb527ade34fce0
Youssef Topaji <youssefftopaji@gmail.com>
2025-09-14 11:59:06 +0200

    Creating InMemoryRepository and RepositoryContracts

---
+++ Server/RepositoryContracts/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface ICommentRepository
+{
+    Task<Comment> AddAsync(Comment comment);
+    Task<Comment> UpdateAsync(Comment comment);
+    Task<Comment> DeleteAsync(int id);
+    Task<Comment> GetSingleAsync(int id);
+    IQueryable<Comment> GetManyAsync();
+}
---
+++ Server/RepositoryContracts/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositoryContracts;
+
+public interface IUserRepository
+{
+    Task<User>  AddAsync(User user);
+    Task<User>  UpdateAsync(User user);
+    Task<User>  DeleteAsync(int id);
+    Task<User>  GetSingleAsync(int id);
+    IQueryable<User> GetManyAsync();
+}
